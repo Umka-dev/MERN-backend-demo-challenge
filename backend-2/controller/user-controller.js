f887a4655eb078feb16d893771c4fd8cf8b85313
Umka-dev <design.tvin@gmail.com>
2025-04-04 18:50:01 +0200

    add new conditions to check all the inputs data

--- backend-2/controller/user-controller.js
+++ backend-2/controller/user-controller.js
@@ -13,14 +13,14 @@ const renderSignUpPage = (req, res) => {
 };
 
 const signUp = async (req, res) => {
-  if (
-    req.body.first_name === '' ||
-    req.body.last_name === '' ||
-    req.body.password === '' ||
-    req.body.email === ''
-  ) {
+  const { first_name, last_name, email, password, confirm_password } = req.body;
+
+  console.log('User inputs: ', req.body);
+
+  // Check all the field not empty
+  if (!first_name || !last_name || !email || !password || !confirm_password) {
     return res.render('signup-login', {
-      signUpErrMessage: 'All the fields should be filled!',
+      signUpErrMessage: 'All fields must be filled!',
       signUpMessage: null,
       noUserMessage: null,
       wrongSignupPassMessage: null,
@@ -28,65 +28,62 @@ const signUp = async (req, res) => {
     });
   }
 
-  // bcrypt/hash the password
-  if (req.body.password !== '') {
-    // Hash password
-    const hashedPass = bcrypt.hashSync(req.body.password, 10);
-    const userData = {
-      ...req.body,
-      password: hashedPass, // replace the real password with the encrypted one
-    };
-    const newUser = new userModel(userData);
-    newUser
-      .save()
-      .then((data) => {
-        res.render('signup-login', {
-          signUpErrMessage: null,
-          signUpMessage: 'The user is signed up. You can log in now.',
-          noUserMessage: null,
-          wrongSignupPassMessage: null,
-          wrongLoginPassMessage: null,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  // Compare two unputed passwords
+  if (password !== confirm_password) {
+    return res.render('signup-login', {
+      signUpErrMessage: 'Passwords do not match!',
+      signUpMessage: null,
+      noUserMessage: null,
+      wrongSignupPassMessage: null,
+      wrongLoginPassMessage: null,
+    });
   }
+
+  // Check if user with this email is alredy exist
+  const existingUser = await userModel.findOne({ email });
+  if (existingUser) {
+    return res.render('signup-login', {
+      signUpErrMessage:
+        'This email is already registered! Please try again with another one.',
+      signUpMessage: null,
+      noUserMessage: null,
+      wrongSignupPassMessage: null,
+      wrongLoginPassMessage: null,
+    });
+  }
+
+  // Hash the password before saving in DB
+  const hashedPass = bcrypt.hashSync(password, 10);
+
+  // Update user data with hashed password
+  const userData = { first_name, last_name, email, password: hashedPass };
+
+  // Create new user in DB and save
+  const newUser = new userModel(userData);
+  newUser
+    .save()
+    .then(() => {
+      res.render('signup-login', {
+        signUpErrMessage: null,
+        signUpMessage: 'User successfully registered. You can log in now.',
+        noUserMessage: null,
+        wrongSignupPassMessage: null,
+        wrongLoginPassMessage: null,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 // Log in
 const logIn = async (req, res) => {
+  const { email, password } = req.body;
   // First: check if the user is exist
-  const existedUser = await userModel.findOne({ email: req.body.email });
+  const existedUser = await userModel.findOne({ email });
 
-  if (existedUser) {
-    // Second: check if the password is correct
-    const isCorrectPass = bcrypt.compareSync(
-      req.body.password,
-      existedUser.password
-    );
-    if (isCorrectPass) {
-      const userInfo = {
-        id: existedUser._id,
-        first_name: existedUser.first_name,
-        last_name: existedUser.last_name,
-        email: existedUser.email,
-      };
-      const userToken = await jwt.sign({ userInfo }, 'User is JWT now');
-
-      res.cookie('authToken', userToken); // register user token inside the cookie
-      return res.redirect('/user');
-    } else {
-      res.render('signup-login', {
-        signUpErrMessage: null,
-        signUpMessage: null,
-        noUserMessage: null,
-        wrongSignupPassMessage: null,
-        wrongLoginPassMessage: 'The password is not correct',
-      });
-    }
-  } else {
-    res.render('signup-login', {
+  if (!existedUser) {
+    return res.render('signup-login', {
       signUpErrMessage: null,
       signUpMessage: null,
       noUserMessage: 'The user does not exist. Sign up first, please.',
@@ -94,6 +91,35 @@ const logIn = async (req, res) => {
       wrongLoginPassMessage: null,
     });
   }
+
+  // Second: check if the password is correct
+  const isCorrectPass = await bcrypt.compare(password, existedUser.password);
+  if (!isCorrectPass) {
+    return res.render('signup-login', {
+      signUpErrMessage: null,
+      signUpMessage: null,
+      noUserMessage: null,
+      wrongSignupPassMessage: null,
+      wrongLoginPassMessage: 'The password is not correct',
+    });
+  }
+
+  // Third: create a token
+  const userInfo = {
+    id: existedUser._id,
+    first_name: existedUser.first_name,
+    last_name: existedUser.last_name,
+    email: existedUser.email,
+  };
+  const userToken = jwt.sign({ userInfo }, 'User is JWT now', {
+    expiresIn: '24h',
+  });
+
+  //Fourth: set token into the cookie
+  res.cookie('authToken', userToken);
+
+  // Lead to user page
+  return res.redirect('/user');
 };
 
 // Get user data from the cookies
